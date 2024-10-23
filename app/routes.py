1cb5bc32a6ededeb3c907c65451a95111bade175
excretory <elstxds@proton.me>
2024-10-23 22:33:50 +0500

    templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    user = {
+        'username': 'exy'
+    }
+    posts = [
+        {
+            'author': {'username': 'Jhon'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
